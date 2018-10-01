81d95ed00e0a0e84bcacd5ea96c343a5d8e83c5e
Naoyuki Miyazaki <miyazaki@buddying.jp>
2018-10-01 11:28:52 +0900

    Add Code highlight

--- src/Code.js
+++ src/Code.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
+import hljs from 'highlightjs'
 
 import './Code.css'
+import 'highlightjs/styles/default.css'
 
-const Code = ({ html }) => (
-  <pre className="code">
-    <code className="code__inner">{html}</code>
-  </pre>
-)
+class Code extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      highlight: {},
+    }
+  }
+
+  componentDidMount() {
+    if (!this.props.html) return
+
+    this.highlight()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.html === prevProps.html) return
+
+    this.highlight()
+  }
+
+  highlight() {
+    this.setState({
+      highlight: hljs.highlight('html', this.props.html)
+    })
+  }
+
+  render() {
+    const { highlight } = this.state
+
+    return (
+      <pre className="code">
+        <code className="code__inner html" dangerouslySetInnerHTML={{__html: highlight.value}} />
+      </pre>
+    )
+  }
+}
 
 export default Code
